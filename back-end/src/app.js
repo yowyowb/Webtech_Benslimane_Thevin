461dced8b61578de82ccbbac57ecefbc0e712bdc
Bertrand Vautrin <bertrand.vautrin@epitech.eu>
2020-12-16 21:49:31 +0100

    fix messges

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -20,18 +20,4 @@ app.get('/', (req, res) => {
   ].join(''))
 })
 
-// Messages
-
-app.get('/channels/:id/messages', async (req, res) => {
-  const messages = await db.messages.list(req.params.id)
-  res.json(messages)
-})
-
-app.post('/channels/:id/messages', async (req, res) => {
-  const message = await db.messages.create(req.params.id, req.body)
-  res.status(201).json(message)
-})
-
-// Users
-
 module.exports = app
